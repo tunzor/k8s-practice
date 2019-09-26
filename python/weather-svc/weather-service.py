57d5723fa9e2d4de69655345378ed575117aeab4
Anthony <russo555@gmail.com>
2019-09-25 22:20:03 -0400

    Added python weather service with dockerfile.

---
+++ python/weather-svc/weather-service.py
@@ -0,0 +1,32 @@
+from flask import Flask
+import requests
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'City required in URL request /{CITY}'
+
+
+@app.route('/<city>')
+def weather(city):
+    locationEndpoint = 'https://www.metaweather.com/api/location/search/?query={}'.format(city)
+
+    locationData = requests.get(locationEndpoint)
+    if locationData.text == '[]':
+        return 'City not found'
+    else:
+        cityId = locationData.json()[0]["woeid"]
+
+        weatherEndpoint = 'https://www.metaweather.com/api/location/{}'.format(cityId)
+        weatherData = requests.get(weatherEndpoint).json()
+
+        weather_data = []
+        for date in weatherData['consolidated_weather']:
+            weather_data.append({'date': date['applicable_date'], 'temperature': date['the_temp'], 'weather_state': date['weather_state_name']})
+        return json.dumps(weather_data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
